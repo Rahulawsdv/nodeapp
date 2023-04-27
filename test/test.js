06fd8f55762b3ca2728a4e3e8f1a74e00fb81983
rahul <rahul@gmail.com>
2023-04-27 18:29:36 +0530

    newwala

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 var app = require('../index.json');describe('GET /', function() {
  it('respond with hello world', function(done) { //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hello Family', done);
+ request(app).get('/').expect('hello JOCKER', done);
  });
 });
